14e76dfad74e7a48c97ed26f2d1a785fbad54b62
Andreas Merz <amerz@maxd>
2018-05-28 00:58:31 +0200

    major rewrite, add data export

--- teslogger.sh
+++ teslogger.sh
@@ -2,42 +2,206 @@
 #
 # teslogger.sh
 #
-# sample energy streams from Telsa Powerwall Gateway and log data to file
+# Purpose: 
+#   sample energy streams from Tesla Powerwall Gateway and log data to file
 #
-# see also: jshon   - tool for parsing JSON data on the command-line
+# Usage examples:
+#
+#   # record energy data
+#   teslogger.sh
+#
+#   # export and understand the structure of the data
+#   teslogger.sh -e outformat="" pattern="" teslog_2018*.json.gz | head -n 200 
+#
+#   # export data to matlab/octave array
+#   teslogger.sh -e teslog_2018-05-26.json.gz | head -n 30 > e.dat
+#   teslogger.sh -e teslog_2018*.json.gz | head -n 30 > e.dat
+#   teslogger.sh -e teslog_2018*.json.gz  > e.dat
+#   teslogger.sh -e pattern='"date_time\|instant_power"' teslog_2018*.json.gz > p.dat
+#
+#   # gnu octave graph
+#   load 'p.dat' ; figure, plot(p(:,7:10)
+#
+# Background:
+#   - connect with browser to Tesla Powerwall 2 and watch IP traffic
+#     to learn how it works
+#   - see also: jshon   - tool for parsing JSON data on the command-line
 
-# settings
-tsamp=5     # sampling interval
+# $Header: teslogger.sh, v1.2, Andreas Merz, 2018, GPL $
+
+hc=cat                     # header filter: none               
+
+#--- default settings ---
+tsamp=5     # sampling interval in seconds, 0 < tstamp < 60
 url1=http://192.168.2.9/api/meters/aggregates   # Tesla adress of Metering info
 url2=http://192.168.2.9/api/system_status/soe   # Battery level in percent
+logfile=aggregates
+action=log,stamp           # default action: start logging, add PC timestamp
+outformat="dat"            # output file format [dat | csv | "" ]
+pattern="date_time\|energy_"
+reject="busway_\|frequency_\|generator_"
 
-Told=$(date +%F,%T.%N)
-mv aggregates.json aggregates_$Told.json   # save any unfinished data
+#--- process arguments ---
+cmdline="$0 $@"
+narg=$#
+echo=echo
+cnt=0
 
-while true ; do
+while [ "$1" != "" ] ; do
+  case "$1" in
+   -h)       sed -n '2,/^#.*Header: /p' $0 | cut -c 2- ; exit ;;   # help
+   -t)       t=echo ; echo="echo -ne \\c" ;;  # dry run, test
+   -e)       action=extract ; hc="sed -e s/^/#/" ;;     # extract
+   -l)       action=log ;;      # log, no pc timestamps
+   -w)       action=watch ;;    # observe
+   -*)       echo "warning: unknown option $1" ;  sleep 2 ;;
+   *=*)      echo $1 | $hc ; eval $1 ;;
+   *)        par="$1" ; cnt=`expr $cnt + 1` ; echo "arg[$cnt]=$par" | $hc ;;
+  esac
+  shift
 
-  # wait to the next multiple of tsamp seconds
-  wait=$(date +%S.%N | sed -e 's/^0//' |
-    awk -v tsamp=$tsamp '{ printf("%f", tsamp -0.007 - ($1 % tsamp))}')
-  sleep $wait
-  Tnew=$(date +%F,%T.%N)
-  dnew=$(echo $Tnew | sed -e 's/,.*//')    # extract new date
-  dold=$(echo $Told | sed -e 's/,.*//')    # extract old date
-
-  # fetch new energy state
-  echo -n "$Tnew: " >> aggregates.json     # add PC timestamp
-  #curl $url1 >> aggregates.json
-  wget -O - $url1   >> aggregates.json
-  echo              >> aggregates.json
-  
-  # when a new day starts, save and compress data
-  if [ "$dold" != "$dnew" ] ; then
-    mv aggregates.json teslog_$dold.json
-    gzip teslog_$dold.json
-    ls -l teslog_$dold.*
-  fi
-
-  Told=$Tnew
+  # compatibility to enumerated parameter interface  - no mixing, only appending of new will work!
+  case "$cnt" in 
+   0)  ;; 
+   1)  logfile="$par" ; logfiles="$logfile" ;;
+   *)  logfiles="$logfiles $par" ;;
+  esac 
 done
 
-  
+logfile=$logfile$t   # avoid overwriting of files in test case
+
+# show settings
+varlist="$(sed -ne "/^#--- default settings ---/,/^#--- process arguments ---/p" $0 | grep "=" | grep -v "^if " | grep -v "^ *echo "  | sed -e 's/=.*//' -e 's/^ *//' | grep -v "#" | sort -u )"
+#echo $varlist
+echo                    | $hc
+echo "# command line:"  | $hc
+echo "$cmdline"         | $hc
+echo                    | $hc
+echo "# settings:"      | $hcc
+for vv in $varlist ; do
+  echo "$vv=\"${!vv}\"" | $hc
+done                    
+echo                    | $hc
+
+#-------------------------------------------------
+# logger loop
+#-------------------------------------------------
+if echo "$action" | grep "log" > /dev/null ; then
+
+  Told=$(date +%F,%T.%N)
+  $t mv $logfile.json ${logfile}_$Told.json   # save any unfinished data
+
+  while true ; do
+
+    # wait to the next multiple of tsamp seconds
+    wait=$(date +%S.%N | sed -e 's/^0//' |
+      awk -v tsamp=$tsamp '{ printf("%f", tsamp -0.007 - ($1 % tsamp))}')
+    sleep $wait
+    Tnew=$(date +%F,%T.%N)
+    dnew=$(echo $Tnew | sed -e 's/,.*//')    # extract new date
+    dold=$(echo $Told | sed -e 's/,.*//')    # extract old date
+
+    # fetch new energy state
+    if echo $action | grep "stamp" > /dev/null ; then
+      echo -n "$Tnew: " >> $logfile.json     # add PC timestamp
+    fi
+    #curl $url1 >> $logfile.json
+    $t wget -O - $url1   >> $logfile.json
+    echo              >> $logfile.json
+
+    # when a new day starts, save and compress data
+    if [ "$dold" != "$dnew" ] ; then
+      $t mv $logfile.json ${logfile}_$dold.json
+      $t gzip ${logfile}_$dold.json
+      $t ls -l ${logfile}_$dold.*
+    fi
+
+    Told=$Tnew
+  done
+
+fi
+
+#-------------------------------------------------
+# observer loop
+#-------------------------------------------------
+if echo "$action" | grep "watch" > /dev/null ; then
+  echo "not implemented yet"
+fi  
+
+#-------------------------------------------------
+# extract data
+#-------------------------------------------------
+if echo "$action" | grep "extract" > /dev/null ; then
+  for logf in $logfiles ; do
+    cat=cat
+    if file $logf | grep "gzip compressed data" | $hc ; then
+      cat=zcat
+    fi
+    if echo "$action" | grep "pretty-print" > /dev/null ; then
+      # simple json formatter
+      $t $cat $logf | tr ',' '\n' |
+	 sed -e 's/{/\n{\n/g'     | sed -e 's/}/\n}\n/g' |
+	 awk '   
+           /\}/  { ident--; }
+        	 { if(0) print $0;
+	           if(1) {fmt=sprintf("%%%ds%%s\n",2*ident); printf(fmt,"",$0)};
+		 };
+           /\{/  { ident++; }
+	   '
+    else
+      # simple json parser
+      $t $cat $logf | tr ',' '\n' |  
+	 sed -e 's/{/\n{\n/g'     | sed -e 's/}/\n}\n/g' |
+	 awk '          
+	                   { if(0) print $0; }   # debug
+           /\{/            { ident++; }
+           /\}/            { ident--; }
+	   /^[0-9]{4}-[0-9]{2}-[0-9]{2}/ { 
+	                     pcdate=$1;
+	                     gsub("-"," ", pcdate); 
+			   }
+	   /^[0-9:\.]+: $/ { pctime=$1;
+	                     gsub(":"," ", pctime); 
+			     date_time=sprintf("\"%s %s\"", pcdate, pctime);
+			     if (ident<1)
+			       print "\ndate_time = " date_time ;
+			   }
+	   /.[A-z].*:/     { if(0) print $0;
+	        	     split($1, t, ":", seps);
+	        	     key=t[1];
+			     val=t[2];
+			     # last_communication_time special treatment:
+			     if(t[5] != "") val=sprintf("%s:%s:%s:%s",t[2],t[3],t[4],t[5]);
+			     if (val == "") 
+			       obj=key ;
+			     else { 
+			       k=sprintf("%s_%s",obj,key);  # create a unique key
+			       gsub("\"","",k);             # remove quotes
+			       v[k]=val;
+			     }
+	        	     if(k != "") print k " = " v[k] ;
+			   }
+	   ' | grep -v "$reject" | grep "$pattern" |
+	 awk -F "=" -v format=$outformat '
+                       { newline=0; if(0) print $0; }
+	   /date_time/ { newline=1; }
+	               { if( format == "" ) 
+		            print $0;
+		       }         
+	               { if( format == "dat" ) {    # Matlab ascii data
+		            if(newline) {
+			      newline=0
+			      gsub("\"","",$2);
+			      if(cnt++ == 1) print title ;
+			      print data ;
+			      title="#"
+			      data=""
+			    }
+			    title=sprintf("%s %s", title, $1);
+			    data =sprintf("%s %s",  data, $2);
+		         }
+		       }         
+	               '
+    fi
+  done 
+fi  
